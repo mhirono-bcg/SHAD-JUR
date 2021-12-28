2179b4b10fca2a70552bf4df17159d8963e4b31e
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2021-12-28 15:14:27 +0900

    updated the setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="src",
+    version="1.0.0",
     packages=find_packages(),
-    version="1.0.0f",
-    description="XXX",
     author="Hirono, Masatake",
     author_email="masatakehirono1351@gmail.com",
     license="MIT",
+    entry_points={"console_scripts": ["generate_jur_ss_outputs=src.core:cli"]},
 )
