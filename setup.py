7d88f2ea7bed08562c47f61631094173b8f9921f
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2021-12-28 09:54:56 +0900

    update the setup file

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     packages=find_packages(),
     version="1.0.0f",
     description="XXX",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     author="Hirono, Masatake",
     author_email="masatakehirono1351@gmail.com",
     license="MIT",
